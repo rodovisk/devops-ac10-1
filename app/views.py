8156e7bfb3d19a4a7e1892f250aab5b90440a5d8
rodovisk <42079919+rodovisk@users.noreply.github.com>
2018-11-10 13:00:43 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -69,10 +69,10 @@ def cadastro_vestibulares(request):
     assert isinstance(request, HttpRequest)
     return render(
     request,
-    "app/cadastro_vestibulares.html’,
+    "app/cadastro_vestibulares.html",
     context_instance = RequestContext(request,
     {
-     "title":"Cadastro de vestibulares’,
+     "title":"Cadastro de vestibulares",
      "vestibulares": Vestibular.objects.all(),
      "year":datetime.now().year,
     })
