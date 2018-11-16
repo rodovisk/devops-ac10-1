01e38f56b500f824fb4fa19f00231564241c5717
augustoferreira <42285552+augustoferreira@users.noreply.github.com>
2018-11-16 13:58:25 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ Definition of views.
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
-from app.models import cursos, candidatos
+from app.models import cursos 
 from datetime import datetime
 
 def home(request):
