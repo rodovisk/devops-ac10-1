bce8aa036d4cbac277e93a8b116325c1dfd27bf4
augustoferreira <42285552+augustoferreira@users.noreply.github.com>
2018-11-16 11:42:44 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
-class Curso(models.Model):
+class Candidatos(models.Model):
     nome = models.CharField(max_length=200)
     periodo = models.CharField(max_length=50)
     instituicao = models.CharField(max_length=200)
